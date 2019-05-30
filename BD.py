1587bb4a808fe3e900ed3321a152e788204ecce5
Vinicius <viniciussds2000@Hotmail.com>
2019-05-30 12:21:28 -0300

    Adicionando imagem na pasta, mas n esta mostrando na tabela

--- BD.py
+++ BD.py
@@ -25,9 +25,14 @@ def excluir_user(cursor,conn,login,senha):
     cursor.execute(f'DELETE FROM `semestral3.`.`usuarios` WHERE usuario = "{login}" and senha = "{senha}"')
     conn.commit()
 
-def incluir_anuncio(cursor,conn,nome,marca,ano,cor,cambio,preço,placa,top10):
-    cursor.execute(f'INSERT into `semestral3.`.`carros` (nome,marca,ano,cor,cambio,preço,placa,top10) VALUES ("{nome}","{marca}","{ano}","{cor}","{cambio}","{preço}","{placa}","{top10}")')
+def incluir_anuncio(cursor,conn,nome,marca,ano,cor,cambio,preço,placa,top10,data):
+    cursor.execute(f'INSERT into `semestral3.`.`carros` (nome,marca,ano,cor,cambio,preço,placa,top10,url) VALUES ("{nome}","{marca}","{ano}","{cor}","{cambio}","{preço}","{placa}","{top10}","{data}")')
     conn.commit()
+
+'''def adicionar_imagem(cursor,conn,data):
+    cursor.execute(f'INSERT INTO carros (url) VALUES ("{data}")')
+    conn.commit()'''
+
 
 def excluir_anuncio(cursor,conn,placa):
     cursor.execute(f'DELETE FROM `semestral3.`.`carros` WHERE placa = "{placa}"')
@@ -36,7 +41,7 @@ def excluir_anuncio(cursor,conn,placa):
 def get_carros(cursor):
 
     # Executar o SQL
-    cursor.execute(f'SELECT carros.nome,carros.marca,carros.ano,carros.cor,carros.cambio,carros.preço,carros.placa,carros.top10 '
+    cursor.execute(f'SELECT * '
                    'FROM carros')
 
     # Recuperando o retorno do BD
@@ -45,8 +50,20 @@ def get_carros(cursor):
     # Retornar os dados
     return carros
 
+def get_carro_id(cursor,numero_carro):
+
+    # Executar o SQL
+    cursor.execute(f'SELECT * FROM carros WHERE id = "{numero_carro}"')
+
+    # Recuperando o retorno do BD
+    carros = cursor.fetchall()
+
+    # Retornar os dados
+    return carros
+
+
 def get_top10(cursor):
-    cursor.execute(f'SELECT carros.nome,carros.marca,carros.ano,carros.cor,carros.cambio,carros.preço,carros.placa '
+    cursor.execute(f'SELECT carros.id,carros.nome,carros.marca,carros.ano,carros.cor,carros.cambio,carros.preço,carros.placa '
                    'FROM carros WHERE top10 = "1"')
 
     top10 = cursor.fetchall()
@@ -58,9 +75,12 @@ def edit_top10(cursor,conn,top,placa):
     conn.commit()
 
 
-def adicionar_imagem(cursor,conn,data):
-    cursor.execute(f'INSERT INTO data VALUES LOAD_FILE("{data}"))')
+
+
+def call_imagem(cursor,conn,carro):
+    cursor.execute(f'SELECT url FROM carros WHERE nome = "{carro}"')
     conn.commit()
+
 
 
 
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from flask import Flask,render_template,request,send_file
+import os
+
+from flask import Flask,flash,render_template,request,redirect,url_for,send_file,send_from_directory
 from flaskext.mysql import MySQL
 from werkzeug.utils import secure_filename
 from BD import *
 
 app = Flask(__name__)
 
+UP_FOLDER = 'imagens\\carros'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 mysql = MySQL()
 
 mysql.init_app(app)
 
+app.config['UP_FOLDER'] = UP_FOLDER
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'semestral3.'
@@ -17,12 +22,13 @@ app.config['MYSQL_DATABASE_DB'] = 'semestral3.'
 def home():
     cursor = mysql.get_db().cursor()
 
-    return render_template("home.html",carros=get_top10(cursor))
+    return render_template("home.html",detail_url='http://127.0.0.1:5000/detalhes/carro/',carros=get_top10(cursor))
 
-@app.route('/detalhes')
-def detalhes_carro():
-    cursor= mysql.get_db().cursor()
-    return render_template('detalhes.html', carros=get_carros(cursor))
+@app.route('/detalhes/carro/<carros>', methods=['GET','POST'])
+def detalhes_carro(carro):
+    conn= mysql.connect()
+    cursor=conn.cursor()
+    return render_template('detalhes.html', carros=get_carro_id(cursor, numero_carro),url=call_imagem(cursor,conn,carro))
 
 
 
@@ -112,6 +118,11 @@ def Incluir_anuncio():
     cursor = mysql.get_db().cursor()
     return render_template('incluir_anuncio.html',carros=get_carros(cursor))
 
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/anuncio_incluso', methods=['GET','POST'])
 def incluindo_anuncio():
     if request.method == 'POST':
@@ -123,26 +134,52 @@ def incluindo_anuncio():
         preçocarro=request.form.get('preçocar')
         placacarro=request.form.get('placacar')
         top10 = request.form.get('top_choice')
+        file = request.form.get('file')
         if top10 == "Sim":
             top10=1
         else:
             top10=0
-        #arquivo = request.files['file']
 
-        conn = mysql.connect()
-        cursor = conn.cursor()
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_location = os.path.join(app.config['UP_FOLDER'], filename)
+            print(filename)
+            print(file_location)
+            print(file_location)
+            escaped_file_location = file_location.replace('\\', '/')
+            print(escaped_file_location)
+            file.save(os.path.join(app.config['UP_FOLDER'], filename))
 
-        #adicionar_imagem(cursor,conn,arquivo)
-        incluir_anuncio(cursor,conn,nomecarro,marcacarro,anocarro,corcarro,cambiocarro,preçocarro,placacarro,top10)
-
-        cursor.close()
-        conn.close()
+            conn = mysql.connect()
+            cursor = conn.cursor()
 
 
+            # adicionar_imagem(cursor,conn,arquivo)
+            incluir_anuncio(cursor, conn, nomecarro, marcacarro, anocarro, corcarro, cambiocarro, preçocarro,
+                            placacarro, top10,escaped_file_location)
 
-        return render_template('incluso.html')
-    else:
-        return render_template('adm_page.html')
+            cursor.close()
+            conn.close()
+
+            return redirect(url_for('uploads_file', filename=filename))
+    return render_template('incluir_anuncio')
+
+@app.route('/upload/<filename>', methods=['GET','POST'])
+def uploads_file(filename):
+
+
+    return send_from_directory(app.config['UP_FOLDER'],
+                               filename)
+
+
+
 
 
 @app.route('/excluir_anuncio')
@@ -194,16 +231,55 @@ def top_editado():
         return render_template('incluso.html')
     else:
         return render_template('adm_page.html')
+'''
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET','POST'])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_location = os.path.join(app.config['UP_FOLDER'], filename)
+            print(file_location)
+            escaped_file_location  = file_location.replace('\\','/')
+            print(escaped_file_location)
+            file.save(os.path.join(app.config['UP_FOLDER'], filename))
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+
+            adicionar_imagem(cursor, conn, filename)
+
+            cursor.close()
+            conn.close()
+
+            return redirect(url_for('uploads_file',filename=filename))
+    return render_template('teste.html')
+
+@app.route('/upload/<filename>', methods=['GET','POST'])
+def uploads_file(filename):
 
 
-
+    return send_from_directory(app.config['UP_FOLDER'],
+                               filename)'''
 
 
 @app.route('/carros_reservados')
 def reservas():
     cursor = mysql.get_db().cursor()
-    return render_template('carros_reservados.html',carros=get_carros(cursor))
-
+    return render_template('carros_reservados.html',carros=get_carros(cursor), base_url='http://127.0.0.1:5000/upload/')
 
 
 
